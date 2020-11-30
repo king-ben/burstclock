e3c0d2eb22d50b4ff161034fc42ddbf3f7b0f08d
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-11-30 21:55:48 +0100

    Generate stats from data

--- supplement/austronesian/filter-austronesian.py
+++ supplement/austronesian/filter-austronesian.py
@@ -8,7 +8,14 @@ austronesian = {
     if l["Family"] == "Austronesian"
 }
 
+countlects = 0
 for i in sys.stdin:
     i = i.strip()
     if i in austronesian:
         print(i)
+        countlects += 1
+
+with open("stats.tex", "w") as s:
+    print(f"""
+    \\newcommand{{countlects}}{{{countlects}}}
+        """, file=s)
