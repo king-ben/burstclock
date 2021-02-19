7d838c622b68adc92c2d61bc3911d2cf9bb83b74
NicoNeureiter <nico.neureiter@gmail.com>
2021-02-19 16:28:55 +0100

    1) renamed SemiStrictClock to BurstClock 2) changed inheritance from StrictClockModel to BranchRateModel.Base

--- beast/src/beast/evolution/branchratemodel/BurstClock.java
+++ beast/src/beast/evolution/branchratemodel/BurstClock.java
@@ -6,18 +6,22 @@ import beast.core.parameter.RealParameter;
 import beast.evolution.tree.Node;
 
 @Description("Clock model with a constant contribution from each split in the tree, on top of the specified ´metaClock´ branch rate model")
-public class SemiStrictClock extends StrictClockModel {
+public class BurstClock extends BranchRateModel.Base {
 
-    public Input<RealParameter> persplitInput = new Input<>("perSplit",
-			"Effective number of changes after each split in the tree",
+    public Input<RealParameter> persplitInput = new Input<>(
+            "perSplit",
+            "Effective number of changes after each split in the tree",
             Input.Validate.REQUIRED);
 
-    public Input<BranchRateModel.Base> metaClockInput = new Input<>("metaClock",
-            "The clock model defining the rate along different branches (ignoring splits).");
+    public Input<BranchRateModel.Base> metaClockInput = new Input<>(
+            "metaClock",
+            "The clock model defining the rate along different branches (ignoring splits).",
+            Input.Validate.XOR, meanRateInput);
 
-	public Input<RealParameter> birthrateInput = new Input<RealParameter>("birthrate",
-			"Birth rate (not effective diversification rate) of the tree, used to infer additional changes after unobserved splits. (Not necessary, but likely makes for better mixing.)",
-			new RealParameter(new Double[] { 0. }));
+    public Input<RealParameter> birthrateInput = new Input<RealParameter>(
+            "birthrate",
+            "Birth rate (not effective diversification rate) of the tree, used to infer additional changes after unobserved splits. (Not necessary, but likely makes for better mixing.)",
+            new RealParameter(new Double[] { 0. }));
 
     BranchRateModel.Base metaClock;
 
@@ -31,8 +35,8 @@ public class SemiStrictClock extends StrictClockModel {
         }
     }
 
-	@Override
-	public double getRateForBranch(final Node node) {
+    @Override
+    public double getRateForBranch(final Node node) {
         double perSplit = persplitInput.get().getValue();
         double birthRate = birthrateInput.get().getValue();
         double metaRate = metaClock.getRateForBranch(node);
@@ -40,5 +44,6 @@ public class SemiStrictClock extends StrictClockModel {
 
         double splits = 1 + blength * birthRate;
         return Math.max(metaRate, (splits * perSplit + blength * metaRate) / blength);
-	}
+    }
+
 }
