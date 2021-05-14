73ce9bddc3d3594f29ccb01daea5952b9258ceec
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2021-05-14 14:28:22 +0200

    Copy elements over from Arawak

---
+++ supplement/strip-markdown.py
@@ -0,0 +1,24 @@
+import lxml.etree as etree
+
+parser = etree.XMLParser()
+
+chunk = ""
+before_start = True
+for line in open("templates/template.xml"):
+    if "```xml" in line:
+        if before_start:
+            chunk = ""
+            line = line[line.index("```xml") + 6 :]
+            before_start = False
+        else:
+            line = line.replace("```xml", "-->")
+    if "```" in line:
+        parser.feed(chunk)
+        chunk = ""
+        line = line.replace("```", "<!--")
+    chunk = chunk + line
+
+
+root = parser.close()
+
+print(etree.tostring(root, pretty_print=True).decode("utf-8"))
