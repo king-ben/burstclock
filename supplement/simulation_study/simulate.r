edace353b2390ff0edecb8d48d0796a14618a298
NicoNeureiter <nico.neureiter@gmail.com>
2021-11-19 19:19:14 +0100

    Added set-up for simulation study

---
+++ supplement/simulation_study/simulate.r
@@ -0,0 +1,99 @@
+# simulate.r
+#
+# Performs tree and sequence simulation together with BEAST analysis
+# and its evaluation.
+library("argparser")
+
+import::from("src/beast.r", process_beast_template)
+import::from("src/utils.r", settmpwd, mkdir, run_beast, run_loganalyser, run_coverage_calculator, run_treeannotator_all)
+import::from("src/seqgen.r", seqgen_sampling)
+
+options(scipen=999)
+
+SAMPLING.DIR = "intermediate/sampling/"
+
+main = function(){
+
+    ntax = 12
+    nsamples = 500
+    simu_sample_interval = 5000
+    repeats = 1
+
+    mkdir("intermediate/sampling")
+    mkdir("intermediate/bursty")
+    mkdir("intermediate/burstfree")
+    mkdir("intermediate/bursty/coverage")
+    mkdir("intermediate/burstfree/coverage")
+
+    taxa = make_taxa_names(ntax)
+    
+    # generate template to sample from prior
+    simulator_params = list(
+        "sample_from_prior" = "true",
+        "taxa" = taxa,
+        "nsamples" = 1 + nsamples,
+        "chain_length" = 1 + simu_sample_interval*nsamples,
+        "sample_interval" = simu_sample_interval
+        )
+    
+    # template_simulator = "templates/direct_simulator.xml"
+    template_simulator = "templates/mcmc_simulator.xml"
+    template_seqgen = "templates/seqgen_and_analysis.xml"
+
+    config_bursty = "templates/bursty.toml"
+    config_burstfree = "templates/burstfree.toml"
+
+    process_beast_template(
+        template_simulator,
+        config_bursty,
+        taxa,
+        "intermediate/sampling/SAMPLING.xml",
+        simulator_params
+        )
+
+    # run beast to sample from prior
+    run_beast("intermediate/sampling/SAMPLING.xml")
+
+    # simulate data and reconstruct with bursts
+    seqgen_sampling(
+        "intermediate/sampling/SAMPLING.log",
+        "intermediate/sampling/SAMPLING.trees",
+        template_seqgen,
+        config_bursty,
+        config_bursty,
+        taxa,
+        "intermediate/bursty/test.xml",
+        list(seqlength="2000"),
+        repeats = repeats
+    )
+
+    # simulate data and reconstruct without bursts
+    seqgen_sampling(
+        "intermediate/sampling/SAMPLING.log",
+        "intermediate/sampling/SAMPLING.trees",
+        template_seqgen,
+        config_burstfree,
+        config_burstfree,
+        taxa,
+        "intermediate/burstfree/test.xml",
+        list(seqlength="2000"),
+        repeats = repeats
+    )
+    
+    # evaluate
+    run_loganalyser("intermediate/bursty/")
+    run_coverage_calculator("intermediate/bursty/")
+
+    run_loganalyser("intermediate/burstfree/")
+    run_coverage_calculator("intermediate/burstfree/")
+    }
+
+make_taxa_names = function(ntax){
+    paste(1:ntax)
+    # make.unique(rep(LETTERS, length.out=ntax), sep="")
+    }
+
+
+if(!interactive()){
+    main()
+    }
---
+++ supplement/simulation_study/src/seqgen.r
@@ -0,0 +1,152 @@
+# seqgen.r
+#
+# simulate sequences based on parameter values
+
+import::here(.from="beast.r", merge, process_beast_template)
+import::here(.from="utils.r", run_beast)
+import::here(.from="stringr", str_pad)
+import::here(.from="icesTAF", mkdir)
+import::here(.from="rlist", list.append)
+import::here(.from="parallel", mclapply)
+import::here(.from="tools", file_path_sans_ext, file_ext)
+
+seqgen = function(seqgen_template,
+                  seqgen_config,
+                  taxa,
+                  output,
+                  parameters = list(),
+                  repeats = 1
+    ){
+    output = normalize_path(output)
+
+    for(i in seq_len(repeats)){
+        # repeat_output = repeat_path(output, i) # some temp path
+        repeat_output = output
+        parameters[["sequence_output"]] = buid_seq_output(repeat_output)
+        process_beast_template(
+            seqgen_template,
+            seqgen_config,
+            taxa,
+            repeat_output,
+            parameters
+            )
+        run_beast(repeat_output)
+        }
+    }
+
+
+seqgen_sampling = function( log,
+                            trees_path,
+                            seqgen_template,
+                            seqgen_config,
+                            merge_config,
+                            taxa,
+                            output,
+                            parameters = list(),
+                            repeats = 1
+    ){
+    log = read_log(log)
+    
+    
+    trees = ape::read.nexus(trees_path)[-1]
+
+    if(nrow(log) != length(trees))
+        stop("ERROR: rows of log should be identical to the number of trees")
+    
+    all.run.args = list()
+    
+    n.taxa = length(taxa)
+    n.nodes = 2 * n.taxa - 1
+
+
+
+    for(i in seq_along(trees)){
+        variables = as.list(log[i,])
+        variables[["tree"]] = ape::write.tree(trees[[i]], "")
+        variables[["taxa"]] = taxa
+        
+        variables = merge(variables, parameters)
+        sampling_output = repeat_path(output, i)
+
+        if ("branchRates.1" %in% names(variables)) {
+            branchRates = list()
+            for (i in 1:(n.nodes-1)) {
+                branchRates = list.append(branchRates, variables[[paste("branchRates", i, sep=".")]])
+            }
+            variables[["branchRates"]] = paste(branchRates, collapse = ' ')
+        }
+
+        run.args = list(vars=variables, out=sampling_output)
+        all.run.args = list.append(all.run.args, run.args)
+    }
+
+    mclapply(X = all.run.args,
+             FUN = function(run.args) seqgen(seqgen_template,
+                                             seqgen_config,
+                                             taxa,
+                                             run.args[['out']],
+                                             run.args[['vars']],
+                                             repeats),
+             mc.cores = 2,
+             mc.preschedule = FALSE,
+             mc.allow.recursive = FALSE)
+    }
+
+
+normalize_path = function(x){
+    wd = getwd()
+    file.path(wd, x)
+    }
+
+
+repeat_path = function(path, i){
+    i_padded = str_pad(i, 3, pad="0")
+    paste(file_path_sans_ext(path), i_padded, file_ext(path), sep=".")
+}
+
+
+buid_seq_output = function(path){
+  path_stripped <- file_path_sans_ext(path)
+  seq_dir <- paste(path_stripped, ".data/", sep="")
+  mkdir(seq_dir)
+  paste(seq_dir, "data.xml", sep="")
+}
+
+
+read_log = function(log){
+    log = read.table(log, header=TRUE, stringsAsFactors=FALSE)
+    log = log[-1,] # first values are starting positions
+    bad_columns = c("Sample", "posterior", "likelihood", "prior")
+    log = log[, !colnames(log) %in% bad_columns]
+    log
+    }
+
+
+to_list = function(vec){
+    # as.list(vec) would be enough if not for multidimensional parameters
+    names = names(vec)
+    dotnames = grep("\\.[0-9]+$", names)
+    dotvec = vec[dotnames]
+    vec = vec[-dotnames]
+    dotlist = to_list_dotvec(dotvec)
+    list = as.list(vec)
+    return(c(vec, dotlist))
+    }
+
+
+to_list_dotvec = function(dotvec){
+    unique_dotnames = unique(sub("\\.[0-9]+$", "", names(dotvec)))
+    dotlist = lapply(unique_dotnames, to_list_dotname, dotvec)
+    names(dotlist) = unique_dotnames
+    dotlist
+    }
+
+
+to_list_dotname = function(name, dotvec){
+    match = unlist(dotvec[grep(name, names(dotvec))])
+    order = strsplit(names(match), split=".", fixed=TRUE)
+    order = lapply(order, getElement, 2)
+    order = as.numeric(unlist(order))
+    names(match) = NULL
+    match[order]
+    }
